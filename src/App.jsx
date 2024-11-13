87d967dbc47e6edebd80d9eff64333ed29d5cff7
franco chipana siguas <chipanasiguas@gmail.com>
2024-11-12 22:33:21 -0500

    configurando libreria de componentes y tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Tienda de dispositivos móviles</h1>
+      <h1 className="text-red-500">Tienda de dispositivos móviles</h1>
     </>
   );
 }
