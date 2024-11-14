8a3c4ea4cc584aeb268b9c804b63e4eb481b0702
franco chipana siguas <chipanasiguas@gmail.com>
2024-11-13 21:32:02 -0500

    pagina not-fount agregada

--- src/view/not-found.jsx
+++ src/view/not-found.jsx
@@ -1,3 +1,25 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { TriangleAlert } from "lucide-react";
+import { Link } from "react-router-dom";
+import { Header } from "../components/header";
+
 export function NotFound() {
-  return <div>not-found</div>;
+  return (
+    <div className="min-h-screen bg-background">
+      <Header />
+
+      <main className="container mx-auto px-4 py-8">
+        <Alert>
+          <TriangleAlert className="h-4 w-4" />
+          <AlertTitle>Error 404</AlertTitle>
+          <AlertDescription>
+            La página solicitada no existe.{" "}
+            <Link to="/productos" className="text-blue-500 font-semibold">
+              Volver al inicio
+            </Link>
+          </AlertDescription>
+        </Alert>
+      </main>
+    </div>
+  );
 }
